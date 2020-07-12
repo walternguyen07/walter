d9599deb1d4ebc0c4fabff9523946fa5ab17fb4c
Walter <johnnguyencodedao@gmail.com>
2020-07-12 23:35:22 +0700

    add theme frontend

--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('front.master');
 });
 Route::get("admin", "back\Homecontroller@index");
 Route::get("login", "LoginController@login");
