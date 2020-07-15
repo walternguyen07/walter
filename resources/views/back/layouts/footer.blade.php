60883f90874b9deaa59e9c346e672b980846ffaf
Walter <johnnguyencodedao@gmail.com>
2020-07-16 05:38:06 +0700

    fix footer

--- resources/views/back/layouts/footer.blade.php
+++ resources/views/back/layouts/footer.blade.php
@@ -1,334 +1,57 @@
 <footer class="main-footer">
-    <div class="pull-right d-none d-sm-inline-block">
-        <ul class="nav nav-primary nav-dotted nav-dot-separated justify-content-center justify-content-md-end">
-		  <li class="nav-item">
-			<a class="nav-link" href="javascript:void(0)">FAQ</a>
-		  </li>
-		  <li class="nav-item">
-			<a class="nav-link" href="#">Purchase Now</a>
-		  </li>
-		</ul>
-    </div>
-	  &copy; 2018 <a href="https://www.multipurposethemes.com/">Multi-Purpose Themes</a>. All Rights Reserved.
-  </footer>
 
-  <!-- Control Sidebar -->
-  <aside class="control-sidebar control-sidebar-light">
-
-	<div class="rpanel-title"><span class="btn pull-right"><i class="ion ion-close" data-toggle="control-sidebar"></i></span> </div>
-    <!-- Create the tabs -->
-    <ul class="nav nav-tabs control-sidebar-tabs">
-      <li class="nav-item"><a href="#control-sidebar-home-tab" data-toggle="tab">Chat</a></li>
-      <li class="nav-item"><a href="#control-sidebar-settings-tab" data-toggle="tab">Todo</a></li>
-    </ul>
-    <!-- Tab panes -->
-    <div class="tab-content">
-      <!-- Home tab content -->
-      <div class="tab-pane" id="control-sidebar-home-tab">
-          <div class="flexbox">
-			<a href="javascript:void(0)" class="text-grey">
-				<i class="ti-more"></i>
-			</a>
-			<p>Users</p>
-			<a href="javascript:void(0)" class="text-right text-grey"><i class="ti-plus"></i></a>
-		  </div>
-		  <div class="lookup lookup-sm lookup-right d-none d-lg-block">
-			<input type="text" name="s" placeholder="Search" class="w-p100">
-		  </div>
-          <div class="media-list media-list-hover mt-20">
-			<div class="media py-10 px-0">
-			  <a class="avatar avatar-lg status-success" href="#">
-				<img src="images/avatar/1.jpg" alt="...">
-			  </a>
-			  <div class="media-body">
-				<p class="font-size-16">
-				  <a class="hover-primary" href="#"><strong>Tyler</strong></a>
-				</p>
-				<p>Praesent tristique diam...</p>
-				  <span>Just now</span>
-			  </div>
-			</div>
-
-			<div class="media py-10 px-0">
-			  <a class="avatar avatar-lg status-danger" href="#">
-				<img src="images/avatar/2.jpg" alt="...">
-			  </a>
-			  <div class="media-body">
-				<p class="font-size-16">
-				  <a class="hover-primary" href="#"><strong>Luke</strong></a>
-				</p>
-				<p>Cras tempor diam ...</p>
-				  <span>33 min ago</span>
-			  </div>
-			</div>
-
-			<div class="media py-10 px-0">
-			  <a class="avatar avatar-lg status-warning" href="#">
-				<img src="images/avatar/3.jpg" alt="...">
-			  </a>
-			  <div class="media-body">
-				<p class="font-size-16">
-				  <a class="hover-primary" href="#"><strong>Evan</strong></a>
-				</p>
-				<p>In posuere tortor vel...</p>
-				  <span>42 min ago</span>
-			  </div>
-			</div>
-
-			<div class="media py-10 px-0">
-			  <a class="avatar avatar-lg status-primary" href="#">
-				<img src="images/avatar/4.jpg" alt="...">
-			  </a>
-			  <div class="media-body">
-				<p class="font-size-16">
-				  <a class="hover-primary" href="#"><strong>Evan</strong></a>
-				</p>
-				<p>In posuere tortor vel...</p>
-				  <span>42 min ago</span>
-			  </div>
-			</div>
-
-			<div class="media py-10 px-0">
-			  <a class="avatar avatar-lg status-success" href="#">
-				<img src="images/avatar/1.jpg" alt="...">
-			  </a>
-			  <div class="media-body">
-				<p class="font-size-16">
-				  <a class="hover-primary" href="#"><strong>Tyler</strong></a>
-				</p>
-				<p>Praesent tristique diam...</p>
-				  <span>Just now</span>
-			  </div>
-			</div>
-
-			<div class="media py-10 px-0">
-			  <a class="avatar avatar-lg status-danger" href="#">
-				<img src="images/avatar/2.jpg" alt="...">
-			  </a>
-			  <div class="media-body">
-				<p class="font-size-16">
-				  <a class="hover-primary" href="#"><strong>Luke</strong></a>
-				</p>
-				<p>Cras tempor diam ...</p>
-				  <span>33 min ago</span>
-			  </div>
-			</div>
-
-			<div class="media py-10 px-0">
-			  <a class="avatar avatar-lg status-warning" href="#">
-				<img src="images/avatar/3.jpg" alt="...">
-			  </a>
-			  <div class="media-body">
-				<p class="font-size-16">
-				  <a class="hover-primary" href="#"><strong>Evan</strong></a>
-				</p>
-				<p>In posuere tortor vel...</p>
-				  <span>42 min ago</span>
-			  </div>
-			</div>
-
-			<div class="media py-10 px-0">
-			  <a class="avatar avatar-lg status-primary" href="#">
-				<img src="images/avatar/4.jpg" alt="...">
-			  </a>
-			  <div class="media-body">
-				<p class="font-size-16">
-				  <a class="hover-primary" href="#"><strong>Evan</strong></a>
-				</p>
-				<p>In posuere tortor vel...</p>
-				  <span>42 min ago</span>
-			  </div>
-			</div>
-
-		  </div>
-
-      </div>
-      <!-- /.tab-pane -->
-      <!-- Settings tab content -->
-      <div class="tab-pane" id="control-sidebar-settings-tab">
-          <div class="flexbox">
-			<a href="javascript:void(0)" class="text-grey">
-				<i class="ti-more"></i>
-			</a>
-			<p>Todo List</p>
-			<a href="javascript:void(0)" class="text-right text-grey"><i class="ti-plus"></i></a>
-		  </div>
-        <ul class="todo-list mt-20">
-			<li class="py-15 px-5 by-1">
-			  <!-- checkbox -->
-			  <input type="checkbox" id="basic_checkbox_1" class="filled-in">
-			  <label for="basic_checkbox_1" class="mb-0 h-15"></label>
-			  <!-- todo text -->
-			  <span class="text-line">Nulla vitae purus</span>
-			  <!-- Emphasis label -->
-			  <small class="badge bg-danger"><i class="fa fa-clock-o"></i> 2 mins</small>
-			  <!-- General tools such as edit or delete-->
-			  <div class="tools">
-				<i class="fa fa-edit"></i>
-				<i class="fa fa-trash-o"></i>
-			  </div>
-			</li>
-			<li class="py-15 px-5">
-			  <!-- checkbox -->
-			  <input type="checkbox" id="basic_checkbox_2" class="filled-in">
-			  <label for="basic_checkbox_2" class="mb-0 h-15"></label>
-			  <span class="text-line">Phasellus interdum</span>
-			  <small class="badge bg-info"><i class="fa fa-clock-o"></i> 4 hours</small>
-			  <div class="tools">
-				<i class="fa fa-edit"></i>
-				<i class="fa fa-trash-o"></i>
-			  </div>
-			</li>
-			<li class="py-15 px-5 by-1">
-			  <!-- checkbox -->
-			  <input type="checkbox" id="basic_checkbox_3" class="filled-in">
-			  <label for="basic_checkbox_3" class="mb-0 h-15"></label>
-			  <span class="text-line">Quisque sodales</span>
-			  <small class="badge bg-warning"><i class="fa fa-clock-o"></i> 1 day</small>
-			  <div class="tools">
-				<i class="fa fa-edit"></i>
-				<i class="fa fa-trash-o"></i>
-			  </div>
-			</li>
-			<li class="py-15 px-5">
-			  <!-- checkbox -->
-			  <input type="checkbox" id="basic_checkbox_4" class="filled-in">
-			  <label for="basic_checkbox_4" class="mb-0 h-15"></label>
-			  <span class="text-line">Proin nec mi porta</span>
-			  <small class="badge bg-success"><i class="fa fa-clock-o"></i> 3 days</small>
-			  <div class="tools">
-				<i class="fa fa-edit"></i>
-				<i class="fa fa-trash-o"></i>
-			  </div>
-			</li>
-			<li class="py-15 px-5 by-1">
-			  <!-- checkbox -->
-			  <input type="checkbox" id="basic_checkbox_5" class="filled-in">
-			  <label for="basic_checkbox_5" class="mb-0 h-15"></label>
-			  <span class="text-line">Maecenas scelerisque</span>
-			  <small class="badge bg-primary"><i class="fa fa-clock-o"></i> 1 week</small>
-			  <div class="tools">
-				<i class="fa fa-edit"></i>
-				<i class="fa fa-trash-o"></i>
-			  </div>
-			</li>
-			<li class="py-15 px-5">
-			  <!-- checkbox -->
-			  <input type="checkbox" id="basic_checkbox_6" class="filled-in">
-			  <label for="basic_checkbox_6" class="mb-0 h-15"></label>
-			  <span class="text-line">Vivamus nec orci</span>
-			  <small class="badge bg-info"><i class="fa fa-clock-o"></i> 1 month</small>
-			  <div class="tools">
-				<i class="fa fa-edit"></i>
-				<i class="fa fa-trash-o"></i>
-			  </div>
-			</li>
-			<li class="py-15 px-5 by-1">
-			  <!-- checkbox -->
-			  <input type="checkbox" id="basic_checkbox_7" class="filled-in">
-			  <label for="basic_checkbox_7" class="mb-0 h-15"></label>
-			  <!-- todo text -->
-			  <span class="text-line">Nulla vitae purus</span>
-			  <!-- Emphasis label -->
-			  <small class="badge bg-danger"><i class="fa fa-clock-o"></i> 2 mins</small>
-			  <!-- General tools such as edit or delete-->
-			  <div class="tools">
-				<i class="fa fa-edit"></i>
-				<i class="fa fa-trash-o"></i>
-			  </div>
-			</li>
-			<li class="py-15 px-5">
-			  <!-- checkbox -->
-			  <input type="checkbox" id="basic_checkbox_8" class="filled-in">
-			  <label for="basic_checkbox_8" class="mb-0 h-15"></label>
-			  <span class="text-line">Phasellus interdum</span>
-			  <small class="badge bg-info"><i class="fa fa-clock-o"></i> 4 hours</small>
-			  <div class="tools">
-				<i class="fa fa-edit"></i>
-				<i class="fa fa-trash-o"></i>
-			  </div>
-			</li>
-			<li class="py-15 px-5 by-1">
-			  <!-- checkbox -->
-			  <input type="checkbox" id="basic_checkbox_9" class="filled-in">
-			  <label for="basic_checkbox_9" class="mb-0 h-15"></label>
-			  <span class="text-line">Quisque sodales</span>
-			  <small class="badge bg-warning"><i class="fa fa-clock-o"></i> 1 day</small>
-			  <div class="tools">
-				<i class="fa fa-edit"></i>
-				<i class="fa fa-trash-o"></i>
-			  </div>
-			</li>
-			<li class="py-15 px-5">
-			  <!-- checkbox -->
-			  <input type="checkbox" id="basic_checkbox_10" class="filled-in">
-			  <label for="basic_checkbox_10" class="mb-0 h-15"></label>
-			  <span class="text-line">Proin nec mi porta</span>
-			  <small class="badge bg-success"><i class="fa fa-clock-o"></i> 3 days</small>
-			  <div class="tools">
-				<i class="fa fa-edit"></i>
-				<i class="fa fa-trash-o"></i>
-			  </div>
-			</li>
-		  </ul>
-      </div>
-      <!-- /.tab-pane -->
-    </div>
-  </aside>
-  <!-- /.control-sidebar -->
-
-  <!-- Add the sidebar's background. This div must be placed immediately after the control sidebar -->
-  <div class="control-sidebar-bg"></div>
+    &copy; 2010 <a href="#">Walter System</a>. All Rights Reserved.
+</footer>
 
 </div>
 <!-- ./wrapper -->
 
 
 
-	<!-- jQuery 3 -->
-	<script src="{{ asset("back/assets/vendor_components/jquery-3.3.1/jquery-3.3.1.js") }}"></script>
+<!-- jQuery 3 -->
+<script src="{{ asset("back/assets/vendor_components/jquery-3.3.1/jquery-3.3.1.js") }}"></script>
 
-	<!-- popper -->
-	<script src="{{ asset("back/assets/vendor_components/popper/dist/popper.min.js") }}"></script>
+<!-- popper -->
+<script src="{{ asset("back/assets/vendor_components/popper/dist/popper.min.js") }}"></script>
 
-	<!-- Bootstrap 4.0-->
-	<script src="{{ asset("back/assets/vendor_components/bootstrap/dist/js/bootstrap.js") }}"></script>
+<!-- Bootstrap 4.0-->
+<script src="{{ asset("back/assets/vendor_components/bootstrap/dist/js/bootstrap.js") }}"></script>
 
-	<!-- Slimscroll -->
-	<script src="{{ asset("back/assets/vendor_components/jquery-slimscroll/jquery.slimscroll.js") }}"></script>
+<!-- Slimscroll -->
+<script src="{{ asset("back/assets/vendor_components/jquery-slimscroll/jquery.slimscroll.js") }}"></script>
 
-	<!-- date-range-picker -->
-	<script src="{{ asset("back/assets/vendor_components/moment/min/moment.min.js") }}"></script>
-	<script src="{{ asset("back/assets/vendor_components/bootstrap-daterangepicker/daterangepicker.js") }}"></script>
+<!-- date-range-picker -->
+<script src="{{ asset("back/assets/vendor_components/moment/min/moment.min.js") }}"></script>
+<script src="{{ asset("back/assets/vendor_components/bootstrap-daterangepicker/daterangepicker.js") }}"></script>
 
-	<!-- Sparkline -->
-	<script src="{{ asset("back/assets/vendor_components/jquery-sparkline/dist/jquery.sparkline.min.js") }}"></script>
+<!-- Sparkline -->
+<script src="{{ asset("back/assets/vendor_components/jquery-sparkline/dist/jquery.sparkline.min.js") }}"></script>
 
-	<!-- easypiechart -->
-	<script src="{{ asset("back/assets/vendor_components/easypiechart/dist/jquery.easypiechart.js") }}"></script>
+<!-- easypiechart -->
+<script src="{{ asset("back/assets/vendor_components/easypiechart/dist/jquery.easypiechart.js") }}"></script>
 
-    <!-- eChart Plugins -->
-    <script src="{{ asset("back/assets/vendor_components/echarts/dist/echarts-en.min.js") }}"></script>
+<!-- eChart Plugins -->
+<script src="{{ asset("back/assets/vendor_components/echarts/dist/echarts-en.min.js") }}"></script>
 
-	<!-- FastClick -->
-	<script src="{{ asset("back/assets/vendor_components/fastclick/lib/fastclick.js") }}"></script>
+<!-- FastClick -->
+<script src="{{ asset("back/assets/vendor_components/fastclick/lib/fastclick.js") }}"></script>
 
-	<!-- This is data table -->
-    <script src="{{ asset("back/assets/vendor_components/datatable/datatables.min.js") }}"></script>
+<!-- This is data table -->
+<script src="{{ asset("back/assets/vendor_components/datatable/datatables.min.js") }}"></script>
 
-	<!-- UltimatePro Admin App -->
-	<script src="{{ asset("back/js/template.js") }}"></script>
-    <script src="{{ asset("back/assets/vendor_components/ckeditor/ckeditor.js") }}"></script>
-    <!-- UltimatePro Admin dashboard demo (This is only for demo purposes) -->
-    <script src="{{ asset("back/assets/vendor_plugins/bootstrap-wysihtml5/bootstrap3-wysihtml5.all.js") }}"></script>
-    <script src="{{ asset("back/js/pages/dashboard2.js") }}"></script>
-    <script src="{{ asset("back/js/pages/editor.js") }}"></script>
+<!-- UltimatePro Admin App -->
+<script src="{{ asset("back/js/template.js") }}"></script>
+<script src="{{ asset("back/assets/vendor_components/ckeditor/ckeditor.js") }}"></script>
+<!-- UltimatePro Admin dashboard demo (This is only for demo purposes) -->
+<script src="{{ asset("back/assets/vendor_plugins/bootstrap-wysihtml5/bootstrap3-wysihtml5.all.js") }}"></script>
+<script src="{{ asset("back/js/pages/dashboard2.js") }}"></script>
+<script src="{{ asset("back/js/pages/editor.js") }}"></script>
 
 
-	<!-- UltimatePro Admin for demo purposes -->
-    <script src="{{ asset("back/js/demo.js") }}"></script>
-        <!-- CK Editor -->
-        <script src="{{ asset("back/assets/vendor_components/dropzone/dropzone.js") }}"></script>
+<!-- UltimatePro Admin for demo purposes -->
+<script src="{{ asset("back/js/demo.js") }}"></script>
+<!-- CK Editor -->
+<script src="{{ asset("back/assets/vendor_components/dropzone/dropzone.js") }}"></script>
 
 
 
