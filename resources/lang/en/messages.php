327ff5f7a649a361e15fef8c998265c6a483582e
Walter <johnnguyencodedao@gmail.com>
2020-07-15 23:51:50 +0700

    fix form blog

---
+++ resources/lang/en/messages.php
@@ -0,0 +1,10 @@
+<?php
+
+return [
+    "Content" => "Nội dung",
+    "Image" => "Hình ảnh",
+    "Save" => "Lưu",
+    " Save &amp; Edit" => "Lưu và tiếp tục",
+    "Publish" => "Publis",
+    "Title" => "Tiêu đề"
+];
--- resources/views/back/blog/add.blade.php
+++ resources/views/back/blog/add.blade.php
@@ -17,68 +17,68 @@
                         <div class="tab-content">
                             <div class="tab-pane active">
                                 <div class="form-group">
-                                    <label for="name" class="control-label required" aria-required="true">Title</label>
+                                    <label for="name" class="control-label required"
+                                        aria-required="true">{{__('messages.Title')}}</label>
                                     <input class="form-control" placeholder="Name" data-counter="120" name="name"
                                         type="text" id="name">
                                 </div>
                                 <div class="form-group">
-                                    <label for="name" class="control-label required" aria-required="true">Image</label>
+                                    <label for="name" class="control-label required"
+                                        aria-required="true">{{__('messages.Image')}}</label>
                                     <form action="#" class="dropzone">
                                         <div class="fallback">
-                                            <input name="file" type="file"  />
+                                            <input name="file" type="file" />
                                         </div>
                                     </form>
                                 </div>
                                 <div class="form-group">
-                                    <label for="content" class="control-label required" aria-required="true">Content</label>
+                                    <label for="content" class="control-label required"
+                                        aria-required="true">{{__('messages.Content')}}</label>
                                     <textarea id="editor1" name="content" rows="10" cols="80">
                                         This is my textarea to be replaced with CKEditor.
                 </textarea>
-          </form>
-                                </div>
-                            </div>
-                            <div class="tab-pane" id="tab_history">
-                                <div class="form-group">
-                                    <label for="name" class="control-label required" aria-required="true">Meta Title</label>
-                                    <input class="form-control" placeholder="Name"  name="meta_title"
-                                        type="text" id="meta_title">
-                                </div>
-                                <div class="form-group">
-                                    <label for="name" class="control-label required" aria-required="true">Meta Keyword</label>
-                                    <input class="form-control" placeholder="Name"  name="meta_keyword"
-                                        type="text" id="meta_keyword">
-                                </div>
-                                <div class="form-group">
-                                    <label for="name" class="control-label required" aria-required="true">Meta Description</label>
-                                    <input class="form-control" placeholder="Name"  name="meta_description"
-                                        type="text" id="meta_description">
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-md-3">
-                    <div class="widget meta-boxes form-actions form-actions-default action-horizontal">
-                        <div class="widget-title">
-                            <h4>
-                                <span>Publish</span>
-                            </h4>
-                        </div>
-                        <div class="widget-body">
-                            <div class="btn-set">
-                                <button type="submit" name="submit" value="save" class="btn btn-info">
-                                    <i class="fa fa-save"></i> Save
-                                </button>
-                                &nbsp;
-                                <button type="submit" name="submit" value="apply" class="btn btn-success">
-                                    <i class="fa fa-check-circle"></i> Save &amp; Edit
-                                </button>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
         </form>
     </div>
+</div>
+<div class="tab-pane" id="tab_history">
+    <div class="form-group">
+        <label for="name" class="control-label required" aria-required="true">Meta Title</label>
+        <input class="form-control" placeholder="Name" name="meta_title" type="text" id="meta_title">
+    </div>
+    <div class="form-group">
+        <label for="name" class="control-label required" aria-required="true">Meta Keyword</label>
+        <input class="form-control" placeholder="Name" name="meta_keyword" type="text" id="meta_keyword">
+    </div>
+    <div class="form-group">
+        <label for="name" class="control-label required" aria-required="true">Meta Description</label>
+        <input class="form-control" placeholder="Name" name="meta_description" type="text" id="meta_description">
+    </div>
+</div>
+</div>
+</div>
+</div>
+<div class="col-md-3">
+    <div class="widget meta-boxes form-actions form-actions-default action-horizontal">
+        <div class="widget-title">
+            <h4>
+                <span>{{__('messages.Publish')}}</span>
+            </h4>
+        </div>
+        <div class="widget-body">
+            <div class="btn-set">
+                <button type="submit" name="submit" value="save" class="btn btn-info">
+                    <i class="fa fa-save"></i> {{__('messages.Save')}}
+                </button>
+                &nbsp;
+                <button type="submit" name="submit" value="apply" class="btn btn-success">
+                    <i class="fa fa-check-circle"></i>{{__('messages. Save &amp; Edit')}}
+                </button>
+            </div>
+        </div>
+    </div>
+</div>
+</div>
+</form>
+</div>
 </div>
 @endsection
