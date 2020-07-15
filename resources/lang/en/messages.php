c73b51bfe8949ecc16d640d126a70128b88dbc9e
Walter <johnnguyencodedao@gmail.com>
2020-07-16 05:15:03 +0700

    refactor struct

--- resources/lang/en/messages.php
+++ resources/lang/en/messages.php
@@ -1,16 +1,25 @@
 <?php
 
 return [
-    "Content" => "Nội dung",
-    "Image" => "Hình ảnh",
-    "Save" => "Lưu",
-    " Save &amp; Edit" => "Lưu và tiếp tục",
-    "Publish" => "Publis",
-    "Title" => "Tiêu đề",
-    "Meta Description" => "Mô tả",
-    "Meta Keyword" => "Từ khóa",
-    "Meta Title" => "Tiêu đề",
-    "List Blog" => "Danh sách bài viết",
-    "Create New Blog" => "Tạo mới bài viết",
-    "Blog" => "Blog"
+    'Content' => 'Nội dung',
+    'Image' => 'Hình ảnh',
+    'Save' => 'Lưu',
+    ' Save &amp; Edit' => 'Lưu và tiếp tục',
+    'Publish' => 'Publis',
+    'Title' => 'Tiêu đề',
+    'Meta Description' => 'Mô tả',
+    'Meta Keyword' => 'Từ khóa',
+    'Meta Title' => 'Tiêu đề',
+    'List Blog' => 'Danh sách bài viết',
+    'Create New Blog' => 'Tạo mới bài viết',
+    'Blog' => 'Blog',
+
+    'Slider' => 'Slider',
+
+    'Banner' => 'Banner',
+    "Shop" => "Cửa hàng",
+    "Product" => "Sản phẩm",
+    "Category" => "Danh mục sản phẩm",
+    "Sytem" => "Hệ thống",
+    "Order" => "Đơn hàng"
 ];
